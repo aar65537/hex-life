d7a91d901dac7277cc0771a88e60879c2f6022d9
Avi Revah <avraham.a.revah@gmail.com>
2025-05-31 20:36:52 +0000

    feat: create canvas component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <script setup>
-  import { onMounted } from "vue";
-
-  onMounted(() => {
-    const canvas = document.querySelector("#gl-canvas")
-    const gl = canvas.getContext("webgl")
-
-    if (gl === null) {
-      alert("Unable to initialize WebGL contex.")
-    }
-    gl.clearColor(0.0, 0.0, 1.0, 1.0)
-    gl.clear(gl.COLOR_BUFFER_BIT)
-  })
+  import Canvas from "./components/Canvas.vue"
 </script>
 
 <template>
-  <canvas id="gl-canvas" width="640" height="480"></canvas>
+  <Canvas></Canvas>
 </template>
-
-<style scoped>
-</style>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { onMounted, useTemplateRef } from "vue"
+
+  const canvas = useTemplateRef("canvas")
+
+  onMounted(() => {
+    console.log(Object.getOwnPropertyNames(canvas));
+    const gl = canvas.value.getContext("webgl")
+
+    if (gl === null) {
+      alert("Unable to initialize WebGL contex.")
+    }
+    gl.clearColor(0.0, 0.0, 1.0, 1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+  })
+</script>
+
+<template>
+  <canvas ref="canvas" width="640" height="480"/>
+</template>
+
