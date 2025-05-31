4a7c3c1ca9ea49fb0b60e32600aad57da8519c64
Avi Revah <avraham.a.revah@gmail.com>
2025-05-31 15:16:25 -0500

    feat: vue app scaffolding

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hex Life
+
+A version of Conway's Game of Life played on a hex grid.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { onMounted } from "vue";
+
+  onMounted(() => {
+    const canvas = document.querySelector("#gl-canvas")
+    const gl = canvas.getContext("webgl")
+
+    if (gl === null) {
+      alert("Unable to initialize WebGL contex.")
+    }
+    gl.clearColor(0.0, 0.0, 1.0, 1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+  })
+</script>
+
+<template>
+  <canvas id="gl-canvas" width="640" height="480"></canvas>
+</template>
+
+<style scoped>
+</style>
