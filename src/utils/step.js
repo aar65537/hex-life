1998e55fb735f1eb1bf1e78bb7fd5adb3406a124
Avi Revah <avraham.a.revah@gmail.com>
2025-06-02 07:28:29 +0000

    feat: its alive

--- src/utils/step.js
+++ src/utils/step.js
@@ -9,9 +9,41 @@ out float cellState;
 
 ${prefix}
 
+Cell neighbor(int index, Cell cell) {
+    if(index == 0){
+        return Cell(cell.q + 1, cell.r);
+    } else if(index == 1) {
+        return Cell(cell.q + 1, cell.r - 1);
+    } else if(index == 2) {
+        return Cell(cell.q, cell.r - 1);
+    } else if(index == 3) {
+        return Cell(cell.q - 1, cell.r);
+    } else if(index == 4) {
+        return Cell(cell.q - 1, cell.r + 1);
+    } else if(index == 5){
+        return Cell(cell.q, cell.r + 1);
+    }
+}
+
+int sumOfNeighbors(Cell cell) {
+    int sum = 0;
+    for(int i = 0; i < 6; i++){
+        Cell neighborCell = neighbor(i, cell);
+        if(inWorld(neighborCell)){
+            sum += isCellAlive(neighborCell) ? 1 : 0; 
+        }
+    }
+    return sum;
+}
+
 void main() {
     Cell cell = Cell(int(gl_FragCoord.x), int(gl_FragCoord.y));
-    cellState = 1.0 - float(isCellAlive(cell));
+    int numOfNeighbors = sumOfNeighbors(cell);
+    if(numOfNeighbors == 2) {
+        cellState = 1.0;
+    } else {
+        cellState = 0.0;
+    }
 }
 `
 
